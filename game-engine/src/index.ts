645c8f2c0951807b27e53e5394af999e89f67360
subramanyac644-ai <subramanyac644@gmail.com>
2026-03-26 15:05:49 +0530

    Restore game-engine standalone plugin and refine Navbar visibility

---
+++ game-engine/src/index.ts
@@ -0,0 +1,7 @@
+export { default as GameEngine } from './GameEngine';
+export { default as ImagePuzzle } from './ImagePuzzle';
+export { default as Tile } from './Tile';
+export * from './utils';
+export type { ImagePuzzleProps } from './ImagePuzzle';
+export type { TileProps } from './Tile';
+export type { TileData } from './utils';
